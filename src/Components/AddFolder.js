adb92163439096e081ee1aedf3ba7333e97b9729
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-12 13:16:44 -0400

    Controlled form validation

--- src/Components/AddFolder.js
+++ src/Components/AddFolder.js
@@ -6,25 +6,43 @@ import api from '../api';
 
 class AddFolder extends React.Component {
   static contextType = DataContext;
-  state={name:""}
+  state = {
+    name: {
+      value: '',
+      touched: false
+    },
 
-  validateAddFolder = (form) => {
-    let formValid = true;
-    if(form.name.value===""){
-      this.setState({name:"Name is required"});
-      formValid = false;
+    hasErrors(){
+      let err = false;
+      let touchCount = 0;
+      let fieldCount = 0;
+      Object.keys(this).forEach(key => {
+        if (key === 'hasErrors') return false;
+        touchCount += this[key].touched ? 1 : 0;
+        fieldCount += 1;
+        if (this[key].error && this[key].touched) err = true;
+      });
+
+      if (fieldCount > touchCount) err = true;
+
+      return touchCount > 0 ? err : true;
     }
- 
-
-    return formValid;
   }
 
-  
+  validate = e => {
+    let input = e.target;
+    this.setState({
+      [input.name]: {
+        value: input.value.trim(),
+        touched: true,
+        error: input.value.trim() === '' ? 'This field is required.' : null
+      },
+    });
+  }
 
   handleSubmit = e => {
     e.preventDefault();
     let form = e.target;
-    if(!this.validateAddFolder(form))return false;
     let folder = {
       id: cuid(),
       name: form.name.value
@@ -39,15 +57,19 @@ class AddFolder extends React.Component {
   render() {
     return (
       <form
+        onChange={this.validate}
         onSubmit={this.handleSubmit}
         onClick={e => e.stopPropagation()}>
         <h2>Add Folder</h2>
         <div>
-          <label htmlFor="name">Name:</label>
+          <label htmlFor="name">
+            Name:
+            <span>{this.state.name.error}</span>
+          </label>
           <input autoComplete="off" type="text" name="name" id="name" placeholder="A Cool Name..."/>
-          <p>{this.state.name}</p>
+          
         </div>
-        <button>Submit</button>
+        <button disabled={this.state.hasErrors()}>Submit</button>
       </form>
     )
   }
--- src/Components/AddNote.js
+++ src/Components/AddNote.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
 import cuid from 'cuid';
 import DataContext from '../DataContext';
 
@@ -7,32 +6,51 @@ import api from '../api';
 
 class AddNote extends React.Component {
   static contextType = DataContext;
-  state={name:"",content:"",folderId:""};
+  state = {
+    name: {
+      value: '',
+      touched: false
+    },
+    content: {
+      value: '',
+      touched: false
+    },
+    folderId: {
+      value: '',
+      touched: false
+    },
 
-  validateAddNote = (form) => {
-    let formValid = true;
-    if(form.name.value===""){
-      this.setState({name:"Name is required"});
-      formValid = false;
-    }
-    if(form.content.value===""){
-      this.setState({content:"Write something"});
-      formValid = false;
-    }
-    if(form.folderId.value===""){
-      this.setState({folderId:"Select a folder"});
-      formValid = false;
-    }
+    hasErrors(){
+      let err = false;
+      let touchCount = 0;
+      let fieldCount = 0;
+      Object.keys(this).forEach(key => {
+        if (key === 'hasErrors') return false;
+        touchCount += this[key].touched ? 1 : 0;
+        fieldCount += 1;
+        if (this[key].error && this[key].touched) err = true;
+      });
 
-    return formValid;
+      if (fieldCount > touchCount) err = true;
+
+      return touchCount > 0 ? err : true;
+    }
   }
 
+  validate = e => {
+    let input = e.target;
+    this.setState({
+      [input.name]: {
+        value: input.value.trim(),
+        touched: true,
+        error: input.value.trim() === '' ? 'This field is required.' : null
+      },
+    });
+  }
   
-
   handleSubmit = e => {
     e.preventDefault();
     let form = e.target;
-    if(!this.validateAddNote(form))return false;
     let note ={
       id: cuid(),
       name: form.name.value,
@@ -50,46 +68,39 @@ class AddNote extends React.Component {
   render() {
     return (
       <form
+        onChange={this.validate}
         onSubmit={this.handleSubmit}
         onClick={e => e.stopPropagation()}>
         <h2>Add Note</h2>
         <div>
-          <label htmlFor="name">Name:</label>
+          <label htmlFor="name">
+            Name:
+            <span>{this.state.name.error}</span>
+          </label>
           <input autoComplete="off" type="text" name="name" id="name" placeholder="A Cool Name..."/>
-          <p>{this.state.name}</p>
         </div>
         <div>
-          <label htmlFor="content">Content:</label>
+          <label htmlFor="content">
+            Content:
+            <span>{this.state.content.error}</span>
+          </label>
           <textarea name="content" id="content" placeholder="Your content goes here." cols="300" rows="4"></textarea>
-          <p>{this.state.content}</p>
         </div>
         <div>
-          <label htmlFor="folderId">Folder:</label>
-          <Switch>
-            <Route exact path="/folder/:id" render={props => {
-              const id = props.match.params.id;
-              return (
-                <select disabled id="folderId" name="folderId" value={id}>
-                  <option value={id}>{
-                    this.context.data.folders.find(folder => folder.id === id).name
-                  }</option>
-                </select>
-              )
-            }}/>
-            <Route render={() => (
-              <select id="folderId" name="folderId">
-                <option value="">Select a folder</option>
-                {
-                  this.context.data.folders.map(folder => (
-                    <option key={folder.id} value={folder.id}>{folder.name}</option>
-                  ))
-                }
-              </select>
-            )}/>
-          </Switch>
-          <p>{this.state.folderId}</p>
+          <label htmlFor="folderId">
+            Folder:
+            <span>{this.state.folderId.error}</span>
+          </label>
+          <select id="folderId" name="folderId">
+            <option value="">Select a folder</option>
+            {
+              this.context.data.folders.map(folder => (
+                <option key={folder.id} value={folder.id}>{folder.name}</option>
+              ))
+            }
+          </select>
         </div>
-        <button>Submit</button>
+        <button disabled={this.state.hasErrors()}>Submit</button>
       </form>
     )
   }
